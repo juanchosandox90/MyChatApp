932d5d0a5b17446410bdd100f4108f1cf0fa5e36
juanchosandox90 <juansandoval222@gmail.com>
2020-04-01 09:17:45 -0500

    feature/004_MenuSettingsLogout  - Implement menu for chat app  - Implement log out in the menu options  - Implement settings activity (empty for now)

---
+++ app/src/main/java/com/juansandoval/mychatapp/activities/SettingsActivity.kt
@@ -0,0 +1,13 @@
+package com.juansandoval.mychatapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.juansandoval.mychatapp.R
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
